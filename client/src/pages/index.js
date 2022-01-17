23a4a59fb3d1cd33e9542c9a86dde1dcd9b6d5ee
garethiv <gareth@garethveale.dev>
2022-01-17 11:26:47 +0000

    more index styling into css module

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -5,7 +5,6 @@ import Seo from "../components/seo"
 const IndexPage = ({location}) => (
   <Layout location={location}>
     <Seo title="Home" />
-
       <div className='index-wrapper'>
         <div>
           <h1 className="index-header-left">GARETH</h1>
@@ -15,7 +14,7 @@ const IndexPage = ({location}) => (
         <div>
           <h1 className="index-header-right">VEALE</h1>
           <h1 className="index-header-right-layer2">VEALE</h1>
-        </div>  
+        </div>
       </div>
   </Layout>
 )
