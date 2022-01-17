22ebd8184276654b84f5d709f1ae61c98be41246
garethiv <gareth@garethveale.dev>
2022-01-17 10:15:14 +0000

    wrap layout.js with framer motion component for page transition effects

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -3,13 +3,27 @@ import PropTypes from "prop-types"
 import Header from "./header"
 import Footer from "./footer"
 import "./layout.css"
+import { motion } from "framer-motion"
 
 const Layout = ({ children, location }) => {
   return (
     <>
       <Header location={location} />
-      
-        <main>{children}</main>
+
+        <motion.main
+          initial={{ opacity: 0, x: -200 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 200 }}
+          transition={{
+            type: "spring",
+            mass: 0.35,
+            stiffness: 75,
+            duration: 0.3,
+            ease:"easeOut"
+          }}
+        >
+          {children}
+        </motion.main>
 
       <Footer />
     </>
