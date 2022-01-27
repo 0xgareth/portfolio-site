63b78e87f9752bb711ead49ee35761a3af36071a
gareth-was-here <gareth@garethveale.dev>
2022-01-27 09:12:40 +0000

    add support for variable fonts using fontsource

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,9 @@
 // You can delete this file if you're not using it
 import * as React from "react"
 import {AnimatePresence} from "framer-motion"
+import "@fontsource/montserrat/800.css"
+import "@fontsource/montserrat/100-italic.css";
+import "@fontsource/montserrat/variable.css"
 
 export const wrapPageElement = ({element}) => (
     <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
