360a7a0563cf7c0a562fe5db0d70c182de9361d9
gareth-was-here <gareth@garethveale.dev>
2022-02-01 13:42:27 +0000

    fix header after adding posts

--- src/components/header.js
+++ src/components/header.js
@@ -26,12 +26,12 @@ const Header = ({ location }) => {
             </Link>
           </p>
           {/* <p style={{margin:`0`}}>
-            <Link to="/projects/" className='header-link'>
+            <a href="https://github.com/gareth-xyz" target="_blank" rel="noopener noreferrer" className='header-link'>
               projects
               <svg className='header-svg' viewBox="0 0 70 36">
                 <path d="M6.9739 30.8153H63.0244C65.5269 30.8152 75.5358 -3.68471 35.4998 2.81531C-16.1598 11.2025 0.894099 33.9766 26.9922 34.3153C104.062 35.3153 54.5169 -6.68469 23.489 9.31527" />
               </svg>
-            </Link>
+            </a>
           </p> */}
           <p style={{margin:`0`}}>
             <Link to="/contact/" className='header-link'>
@@ -43,7 +43,7 @@ const Header = ({ location }) => {
           </p>
         </div>
 
-        <div style={{ display: `flex`, justifyContent:`center` }}>
+        <div style={{ display: `flex`, justifyContent:`center`, paddingRight:`1rem`, paddingTop: `1rem` }}>
           {path === '' ? null :
             <p style={{margin:`0`}}>
               <Link to="/" className='header-link'>
